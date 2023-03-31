33c362de8d4ea0f87a3166a20276ba7c12f56257
NguyenTrieuVuong <nguyentrieuvuong2002@gmail.com>
2023-04-01 00:19:46 +0700

    first commit, add new game snake ai

---
+++ src/MenuPanel.java
@@ -0,0 +1,75 @@
+// package Game;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+	
+	int width;
+	int height;
+	JFrame frame;
+	
+	public MenuPanel(int width, int height, JFrame frame) {
+		this.width = width;
+		this.height = height;
+		this.frame = frame;
+		
+		setPreferredSize(new Dimension(width, height));
+		setBackground(Color.BLACK);
+		setLayout(null);
+		
+		JButton sp = new JButton("SinglePlayer");
+		sp.setLayout(null);
+		sp.setBounds((width / 2) - 100, (height / 4) + 100 - 30, 200, 60);
+		sp.setBackground(Color.GREEN);
+		sp.setBorder(BorderFactory.createBevelBorder(0));
+		sp.addActionListener(new SinglePlayerListener(frame));
+		add(sp);
+		
+		JButton mp = new JButton("MultiPlayer");
+		mp.setLayout(null);
+		mp.setBounds((width / 2) - 100, (height / 4) + 175 - 30, 200, 60);
+		mp.setBackground(Color.GREEN);
+		mp.setBorder(BorderFactory.createBevelBorder(0));
+		mp.addActionListener(new MultiplayerListener(frame));
+		add(mp);
+		
+		JButton sAI = new JButton("Single AI");
+		sAI.setLayout(null);
+		sAI.setBounds((width / 2) - 100, (height / 4) + 250 - 30, 200, 60);
+		sAI.setBackground(Color.GREEN);
+		sAI.setBorder(BorderFactory.createBevelBorder(0));
+		sAI.addActionListener(new SingleAIListener(frame));
+		add(sAI);
+		
+		JButton Astar = new JButton("A* AI");
+		Astar.setLayout(null);
+		Astar.setBounds((width / 2) - 100, (height / 4) + 325 - 30, 200, 60);
+		Astar.setBackground(Color.GREEN);
+		Astar.setBorder(BorderFactory.createBevelBorder(0));
+		Astar.addActionListener(new AStarListener(frame));
+		add(Astar);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.RED);
+		g.setFont(new Font("Ink Free", Font.BOLD, 75));
+		FontMetrics metrics = getFontMetrics(g.getFont());
+		g.drawString("SNAKE", (width - metrics.stringWidth("SNAKE"))/2, height / 4);
+	}
+}
+
---
+++ src/MyFrame.java
@@ -0,0 +1,123 @@
+// package Game;
+
+import java.awt.CardLayout;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class MyFrame extends JFrame {
+	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	private JPanel contentPane;
+	private GamePanel gamePanel;
+	private MultiplayerPanel multiplayerPanel;
+	private SingleAIPanel singleAIPanel;
+	private AStarAIPanel aStarAIPanel;
+	private GameOverPanel GOPanel;
+	private GOMultiplayerPanel GOMP;
+	private GOSingleAIPanel GOSAI;
+	private GOAStarPanel GOAS;
+	private MenuPanel MPanel = new MenuPanel(SCREEN_WIDTH, SCREEN_HEIGHT, this);
+	CardLayout cardLayout = new CardLayout();
+
+	public MyFrame() {
+		setTitle("Snake");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		contentPane.setLayout(cardLayout);
+		contentPane.add(MPanel, "Menu Panel"); 
+		setResizable(false);
+		setContentPane(contentPane);
+		pack();
+		setVisible(true);
+		setLocationRelativeTo(null);
+	}
+	
+	public void gameOver(GameOverPanel GOPanel) {
+		this.GOPanel = GOPanel;
+		contentPane.add(GOPanel, "GameOver Panel");
+        cardLayout.next(contentPane);
+	}
+	
+	public void playAgain() {
+		cardLayout.next(contentPane);
+		gamePanel.restart();
+		contentPane.remove(GOPanel);
+		gamePanel.requestFocusInWindow();
+	}
+	
+	public void singlePlayer() {
+		gamePanel = new GamePanel(this, SCREEN_WIDTH, SCREEN_HEIGHT);
+		contentPane.add(gamePanel, "Game Panel");
+        cardLayout.next(contentPane);
+		contentPane.remove(MPanel);
+		gamePanel.requestFocusInWindow();
+	}
+	
+	public void multiplayer() {
+		multiplayerPanel = new MultiplayerPanel(this, SCREEN_WIDTH, SCREEN_HEIGHT);
+		contentPane.add(multiplayerPanel, "Multiplayer Panel");
+        cardLayout.next(contentPane);
+		contentPane.remove(MPanel);
+		multiplayerPanel.requestFocusInWindow();
+	}
+
+	public void gameOverMultiplayer(GOMultiplayerPanel GOMP) {
+		this.GOMP = GOMP;
+		contentPane.add(GOMP, "GameOver Multiplayer Panel");
+        cardLayout.next(contentPane);
+	}
+
+	public void playAgainMultiplayer() {
+		cardLayout.next(contentPane);
+		multiplayerPanel.restart();
+		contentPane.remove(GOMP);
+		multiplayerPanel.requestFocusInWindow();
+	}
+
+	public void singleAI() {
+		singleAIPanel = new SingleAIPanel(this, SCREEN_WIDTH, SCREEN_HEIGHT);
+		contentPane.add(singleAIPanel, "Single AI Panel");
+        cardLayout.next(contentPane);
+		contentPane.remove(MPanel);
+		singleAIPanel.requestFocusInWindow();
+	}
+	
+	public void gameOverSingleAI(GOSingleAIPanel GOSAI) {
+		this.GOSAI = GOSAI;
+		contentPane.add(GOSAI, "GameOver Single AI Panel");
+        cardLayout.next(contentPane);
+	}
+	
+	public void playAgainSingleAI() {
+		cardLayout.next(contentPane);
+		singleAIPanel.restart();
+		contentPane.remove(GOSAI);
+		singleAIPanel.requestFocusInWindow();
+	}
+
+	public void aStarAI() {
+		aStarAIPanel = new AStarAIPanel(this, SCREEN_WIDTH, SCREEN_HEIGHT);
+		contentPane.add(aStarAIPanel, "A* AI Panel");
+        cardLayout.next(contentPane);
+		contentPane.remove(MPanel);
+		aStarAIPanel.requestFocusInWindow();
+	}
+
+	public void gameOverAStar(GOAStarPanel GOAS) {
+		this.GOAS = GOAS;
+		contentPane.add(GOAS, "GameOver A* Panel");
+        cardLayout.next(contentPane);
+	}
+	
+	public void playAgainAStar() {
+		cardLayout.next(contentPane);
+		aStarAIPanel.restart();
+		contentPane.remove(GOAS);
+		aStarAIPanel.requestFocusInWindow();
+	}
+}
